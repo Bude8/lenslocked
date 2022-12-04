b3cd3e9355b9f3cf3f1d8322e042fff35f29347f
Henry Lee <henryblee8@gmail.com>
2022-12-04 22:43:20 +0000

    Add custom routing

--- main.go
+++ main.go
@@ -16,8 +16,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:foo@gmail.com\">foo@gmail.com</a>.")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler) // path, function. / path catches everything unless another handler covers it
+	http.HandleFunc("/", pathHandler) // path, function. / path catches everything unless another handler covers it
 	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000")
 	http.ListenAndServe(":3000", nil)
