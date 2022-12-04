1c095fb655755491cb78d4680442a14ccdab492b
Henry Lee <henryblee8@gmail.com>
2022-12-04 22:34:46 +0000

    Add basic routing and contact page

--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	// Check e.g. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:foo@gmail.com\">foo@gmail.com</a>.")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc) // path, function. / path catches everything unless another handler covers it
+	http.HandleFunc("/", homeHandler) // path, function. / path catches everything unless another handler covers it
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000")
 	http.ListenAndServe(":3000", nil)
 }
