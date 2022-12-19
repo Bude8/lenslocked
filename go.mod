c6abd4e34a771b1583e25d1e3573a189d8ddf667
Henry Lee <henry.lee@bidfx.com>
2022-12-19 15:41:11 +0000

    Add chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Bude8/lenslocked
 
 go 1.19
+
+require github.com/go-chi/chi/v5 v5.0.8 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,21 @@ import (
 	"net/http"
 )
 
+// // Benefit of this struct type
+// // Access to different fields while still having a function that satisfies Handler interface
+// type Router struct{}
+
+// func (Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+// 	}
+// }
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// Check e.g. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -16,20 +31,40 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:foo@gmail.com\">foo@gmail.com</a>.")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1><p>
+    <ul>
+        <li>Is there a free version? Yes! We offer a free trial for 30 days</li>
+    </ul>
+    `)
+}
+
 func pathHandler(w http.ResponseWriter, r *http.Request) {
+	// URL Path vs RawPath
+	// fmt.Fprintln(w, r.URL.Path)
+	// fmt.Fprintln(w, r.URL.RawPath) // allows for encoding/decoding
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler) // path, function. / path catches everything unless another handler covers it
-	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000")
-	http.ListenAndServe(":3000", nil)
+
+	// http.HandleFunc("/", homeHandler)
+	// http.HandleFunc("/contact", contactHandler)
+	// http.ListenAndServe(":3000", nil)
+
+	// http.Handler - interface with the ServeHTTP method
+	// http.HandlerFunc - a function type that accepts same args as ServeHttp method. Also implements http.Handler
+	// This looks like a function call but is actually a type conversion to the http.HandlerFunc type
+	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 }
